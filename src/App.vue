556fc0b789746a0ad7870caab36d273e1235990a
unknown <plaskacz.panda@gmail.com>
2021-12-17 18:39:51 +0100

    Repo init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-row>
+        <v-col>
+          <span class="text-h5">Viva! Interwencje</span>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn
+            href="https://allegro.pl/uzytkownik/Viva_Interwencje"
+            target="_blank"
+            text
+          >
+            <span class="mr-2">Aukcje</span>
+            <v-icon>mdi-open-in-new</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+
+    <v-main>
+      <v-container mt-8>
+        <v-row justify="center">
+          <v-col cols="10" sm="12" md="12" lg="10" xl="8">
+            <price-chart></price-chart>
+          </v-col>
+        </v-row>
+        <v-row class="mt-4" justify="center">
+          <v-btn @click="getAllegroData">getAllegroData</v-btn>
+        </v-row>
+        <v-row class="mt-4" justify="center">
+          {{ allegroData }}
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PriceChart from "./components/PriceChart";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    PriceChart,
+  },
+
+  data: () => ({
+    allegroData: "testing",
+    CLIENT_ID: "cc8c81263a534b61ac72a64498688613",
+    CLIENT_SECRET:
+      "HtAXopcFIa8V4RX2ctmkhx4v1VqOxq6IwoE8nERWvDQcmpAAZepblwwSEi2GSY7E",
+  }),
+
+  mounted() {},
+
+  methods: {
+    getAllegroData() {
+      //let url = "https://api.allegro.pl/sale/categories/2";
+      let encodedSecret = btoa(this.CLIENT_ID + ":" + this.CLIENT_SECRET);
+
+      let urlAuthorization = "https://allegro.pl/auth/oauth/token";
+      return axios
+        .post(
+          urlAuthorization,
+          { grant_type: "client_credentials" },
+          {
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+              "Access-Control-Allow-Origin": "*",
+              'Authorization': "Basic " + encodedSecret,
+            },
+          }
+        )
+        .then((res) => (this.allegroData = res));
+    },
+    async sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    },
+  },
+};
+</script>
---
+++ src/components/PriceChart.vue
@@ -0,0 +1,98 @@
+
+<script>
+import { Bar, mixins } from "vue-chartjs";
+
+const { reactiveProp } = mixins;
+
+export default {
+  extends: Bar,
+
+  mixins: [reactiveProp],
+
+  data() {
+    return {
+      startData: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            pointBackgroundColor: "white",
+            borderWidth: 1,
+            pointBorderColor: "#249EBF",
+            data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100],
+          },
+        ],
+      },
+
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          enabled: true,
+          mode: "single",
+          callbacks: {
+            label: function (tooltipItems) {
+              return "$" + tooltipItems.yLabel;
+            },
+          },
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+        height: 200,
+      },
+    };
+  },
+
+  computed: {},
+
+  methods: {
+    render() {
+      this.renderChart(this.startData, this.options);
+    },
+  },
+
+  mounted() {
+    this.render();
+  },
+};
+</script>
+
+<style scoped>
+</style>
